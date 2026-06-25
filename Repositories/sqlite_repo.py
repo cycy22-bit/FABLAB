3aed11910c5c3a374291c1889c2a004deac9d204
jenovickayembe-afk <jenovic.kayembe@2029.ulc-icam.com>
2026-06-25 02:05:17 +0100

    ee

--- Repositories/sqlite_repo.py
+++ Repositories/sqlite_repo.py
@@ -1,14 +1,37 @@
 import sqlite3
+from sqlite3 import Row
 
-conn = sqlite3.connect("Database/fablab.db")
 
-cursor = conn.cursor()
+class SQLiteRepository:
 
-cursor.execute(
-    "SELECT name FROM sqlite_master WHERE type='table';"
-)
+    def __init__(self, database_path: str):
+        self.database_path = database_path
 
-for table in cursor.fetchall():
-    print(table[0])
+    def get_connection(self):
+        conn = sqlite3.connect(self.database_path)
+        conn.row_factory = Row
+        return conn
 
-conn.close()
+    def fetch_all(self, query: str, params: tuple = ()):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query, params)
+        rows = cursor.fetchall()
+        conn.close()
+        return rows
+
+    def fetch_one(self, query: str, params: tuple = ()):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query, params)
+        row = cursor.fetchone()
+        conn.close()
+        return row
+
+    def execute(self, query: str, params: tuple = ()) -> bool:
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query, params)
+        conn.commit()
+        conn.close()
+        return True
